cfc2d9df426f6deaeb81e2de7e8b5ecd98fcb81b
Axel <axstraube@gmail.com>
2024-08-18 22:42:13 -0700

    Update h1

--- src/section1-Intro/IntroSection.js
+++ src/section1-Intro/IntroSection.js
@@ -5,14 +5,14 @@ import MovingWater from './MovingWater'
 function IntroSection() {
     return (
         <section id='introSection'>
-            <Parallax speed={-20}>
-                <h1>Axel Straube</h1>
-            </Parallax>
             <Parallax speed={4} id='sun'></Parallax>
             <Parallax speed={-40} id='mountain1' className='mountain'></Parallax>
             <Parallax speed={-30} id='mountain2' className='mountain'></Parallax>
             <Parallax speed={-28} id='movingWater'>
                 <MovingWater />
+            </Parallax>
+            <Parallax speed={-44}>
+                <h1><span><span id='h1LetterA'>A</span>xel</span> <span>Straube</span></h1>
             </Parallax>
             <Parallax id='mountain3' className='mountain'></Parallax>
         </section>
