6ec47b6c4a25544f3317fc33d82417639914b82d
Axel <axstraube@gmail.com>
2024-08-18 21:15:37 -0700

    Add MovingWater

--- src/section1-Intro/IntroSection.js
+++ src/section1-Intro/IntroSection.js
@@ -1,5 +1,6 @@
 import { Parallax } from 'react-scroll-parallax';
 import './IntroSection.css';
+import MovingWater from './MovingWater'
 
 function IntroSection() {
     return (
@@ -10,6 +11,9 @@ function IntroSection() {
             <Parallax speed={4} id='sun'></Parallax>
             <Parallax speed={-40} id='mountain1' className='mountain'></Parallax>
             <Parallax speed={-30} id='mountain2' className='mountain'></Parallax>
+            <Parallax speed={-28} id='movingWater'>
+                <MovingWater />
+            </Parallax>
             <Parallax id='mountain3' className='mountain'></Parallax>
         </section>
     );
---
+++ src/section1-Intro/MovingWater.js
@@ -0,0 +1,13 @@
+import './MovingWater.css';
+
+function MovingWater() {
+    return (
+        <div>
+            <div className="water-row"></div>
+            <div className="water-row"></div>
+            <div className="water-row"></div>
+        </div>
+    )
+}
+
+export default MovingWater
