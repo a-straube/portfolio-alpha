6b142c52e570cdc3c9a838d1423fc5e5f3f8c621
Axel <axstraube@gmail.com>
2024-08-21 21:48:13 -0700

    Start bulk img export file

---
+++ src/img/imgs.js
@@ -0,0 +1,9 @@
+import plant1 from './Pngtree-houseplant-pot-6015944-1.png';
+import plant2 from './Pngtree-houseplant-pot-6015944-2.png';
+import axelPic from './index-axel-piano.jpg';
+
+export default {
+    'plant1':plant1,
+    'plant2':plant2,
+    'axelPic':axelPic
+}
