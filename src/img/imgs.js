2ebd67fa46ced86f67cd83234abcc1f161fbd867
Axel <axstraube@gmail.com>
2024-08-23 19:31:21 -0700

    Add to img list

--- src/img/imgs.js
+++ src/img/imgs.js
@@ -1,9 +1,19 @@
 import plant1 from './Pngtree-houseplant-pot-6015944-1.png';
 import plant2 from './Pngtree-houseplant-pot-6015944-2.png';
 import axelPic from './index-axel-piano.jpg';
+import coconutHandLeft from './pexels-cottonbro-studio-4631073-cartoon-left.png';
+import coconutHandRight from './pexels-cottonbro-studio-4631073-cartoon-right.png';
+import pineappleHandLeft from './pexels-shvets-production-7194810-cartoon-left.png';
+import pineappleHandRight from './pexels-shvets-production-7194810-cartoon-right.png';
+import trelloScreenshot from './trello-project-mgmt-screenshot.jpg';
 
 export default {
     'plant1':plant1,
     'plant2':plant2,
-    'axelPic':axelPic
+    'axelPic':axelPic,
+    'coconutHandLeft': coconutHandLeft,
+    'coconutHandRight': coconutHandRight,
+    'pineappleHandLeft': pineappleHandLeft,
+    'pineappleHandRight': pineappleHandRight,
+    'trelloScreenshot': trelloScreenshot
 }
