32da0d394e765efbd20057c6c5e7c861cdf01ac7
Axel <axstraube@gmail.com>
2024-08-31 13:47:30 -0700

    Add connect icons

--- src/section4-Connect/ConnectSection.js
+++ src/section4-Connect/ConnectSection.js
@@ -1,3 +1,6 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
+import { IoIosMail } from "react-icons/io";
+
 function ConnectSection() {
     return (
         <section id="connectSection">
@@ -5,9 +8,9 @@ function ConnectSection() {
             <div>
                 <span>If you have any questions, project inquiries, or just a good joke to share, feel free to reach out to me via the following channels:</span>
                 <ul id="connectList">
-                    <li><a href="mailto:axeldevdesign@gmail.com" title="Send Axel Straube an email"></a></li>
-                    <li><a href="https://www.linkedin.com/in/axelstraube/" title="Connect with Axel Straube on LinkedIn" target="_blank"></a></li>
-                    <li><a href="https://github.com/a-straube" title="Follow Axel Straube on GitHub" target="_blank"></a></li>
+                    <li><a href="mailto:axeldevdesign@gmail.com" title="Send Axel Straube an email"><IoIosMail /></a></li>
+                    <li><a href="https://www.linkedin.com/in/axelstraube/" title="Connect with Axel Straube on LinkedIn" target="_blank"><FaLinkedin /></a></li>
+                    <li><a href="https://github.com/a-straube" title="Follow Axel Straube on GitHub" target="_blank"><FaGithub /></a></li>
                 </ul>
                 <span className="new-line-span">I look forward to hearing from you and potentially working with you on your next project!</span>
             </div>
