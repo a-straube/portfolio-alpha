712c1039d9150b2b012e7bae42286d4bd8e19b2b
Axel <axstraube@gmail.com>
2024-09-08 00:09:29 -0700

    Remove demo sections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import React, { useLayoutEffect, useRef } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useGSAP } from '@gsap/react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import IntroSection from './section1-Intro/IntroSection';
 import AboutMeSection from './section2-About/AboutMeSection';
@@ -10,43 +7,13 @@ import ConnectSection from './section4-Connect/ConnectSection';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import './global-styles.css';
 
-gsap.registerPlugin(useGSAP, ScrollTrigger);
-
 export default function Scroll() {
-  const main = useRef();
-
-  useGSAP(
-    () => {
-      const boxes = gsap.utils.toArray('.box');
-      boxes.forEach((box) => {
-        gsap.to(box, {
-          x: 150,
-          scrollTrigger: {
-            trigger: box,
-            start: 'bottom bottom',
-            end: 'top 20%',
-            scrub: true,
-            // markers: true,
-          },
-        });
-      });
-    },
-    { scope: main }
-  );
-
   return (
     <ParallaxProvider>
       <IntroSection />
       <AboutMeSection />
       <ProjectsSection />
       <ConnectSection />
-      <div className="section flex-center column" ref={main}>
-        <div className="box gradient-blue">box</div>
-        <div className="box gradient-blue">box</div>
-        <div className="box gradient-blue">box</div>
-      </div>
-      <section className="section"></section>
     </ParallaxProvider>
-    
   );
 }
