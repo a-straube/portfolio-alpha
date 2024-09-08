c13f804505503c382f29ce3f12f53ee1a4a53b9b
Axel <axstraube@gmail.com>
2024-09-07 22:29:15 -0700

    Add GSAP movement to water

--- src/global-components/MovingWater.js
+++ src/global-components/MovingWater.js
@@ -1,17 +1,38 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 import './MovingWater.css';
 
 function MovingWater({ id }) {
+    useGSAP(() => {
+        gsap.to('.water-row--odd', {
+            duration: 25,
+            x: 250,
+            repeat: -1,
+            repeatDelay: 0,
+            ease: "sine.inOut",
+            yoyo: true,
+        })
+        gsap.to('.water-row--even', {
+            duration: 25,
+            x: -250,
+            repeat: -1,
+            repeatDelay: 0,
+            ease: "sine.inOut",
+            yoyo: true,
+        })
+    })
+
     return (
         <div id={id}>
-            <div className='water-row'></div>
-            <div className='water-row'></div>
-            <div className='water-row'></div>
-            <div className='water-row'></div>
-            <div className='water-row'></div>
-            <div className='water-row'></div>
-            <div className='water-row'></div>
-            <div className='water-row'></div>
-            <div className='water-row'></div>
+            <div className='water-row water-row--odd'></div>
+            <div className='water-row water-row--even'></div>
+            <div className='water-row water-row--odd'></div>
+            <div className='water-row water-row--even'></div>
+            <div className='water-row water-row--odd'></div>
+            <div className='water-row water-row--even'></div>
+            <div className='water-row water-row--odd'></div>
+            <div className='water-row water-row--even'></div>
+            <div className='water-row water-row--odd'></div>
         </div>
     )
 }
