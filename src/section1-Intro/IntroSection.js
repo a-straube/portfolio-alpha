a743fdb6c621a39567df16f12accaf229ebf33c6
Axel <axstraube@gmail.com>
2024-10-18 21:36:54 -0700

    Add GSAP movement to clouds

--- src/section1-Intro/IntroSection.js
+++ src/section1-Intro/IntroSection.js
@@ -1,8 +1,33 @@
 import { Parallax } from 'react-scroll-parallax';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 import './IntroSection.css';
 import MovingWater from '../global-components/MovingWater'
 
 function IntroSection() {
+    useGSAP(() => {
+        gsap.to('#cloud1', {
+            duration: 5,
+            x: -15,
+            y: -5,
+            scale: 1.05,
+            repeat: -1,
+            repeatDelay: 0,
+            ease: "power1.in",
+            yoyo: true,
+        })
+        gsap.to('#cloud2', {
+            duration: 15,
+            x: 60,
+            y: -10,
+            scale: 1.2,
+            repeat: -1,
+            repeatDelay: 0,
+            ease: "power1.in",
+            yoyo: true,
+        })
+    })
+
     return (
         <section id='introSection'>
             <Parallax speed={4} id='sun'></Parallax>
@@ -15,7 +40,7 @@ function IntroSection() {
                 <h1><span><span id='h1LetterA'>A</span>xel</span> <span>Straube</span></h1>
             </Parallax>
             <Parallax speed={-24} id='h2'>
-                <h2><span>Front-End</span> <span>Developer</span></h2>
+                <h2><span id="cloud1">Front-End</span> <span id="cloud2">Developer</span></h2>
             </Parallax>
             <Parallax speed={-8} id='mountain3' className='mountain'></Parallax>
         </section>
